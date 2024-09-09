7b265cbffd9f90e6deeb3417f4b63cf9276ff554
Aishie123 <aishalimdones@yahoo.com>
2024-09-09 22:38:50 +0800

    centered overview cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -63,7 +63,7 @@ const About = () => {
         extraordinary together! </b>
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-10 justify-center'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
